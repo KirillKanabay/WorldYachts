406e4e2db5cd2128eda1bd0740e6f2aad726cda1
Kirill Kanabay <kanabay.dev@gmail.com>
2021-01-30 11:10:33 +0300

    Добавлен класс работы с цветом

---
+++ WorldYachts/Helpers/ColorWorker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Media;
+
+namespace WorldYachts.Helpers
+{
+    class ColorWorker
+    {
+        public static Dictionary<string, Color> ColorsDictionary = new Dictionary<string, Color>()
+        {
+            {"Коричневый", GetColorFromString("#8C6354")},
+            {"Белый", GetColorFromString("#FFFFFF")},
+            {"Черный", GetColorFromString("#2A2A2A")},
+            {"Зеленый", GetColorFromString("#4CAF50")},
+            {"Красный", GetColorFromString("#F44336")},
+            {"Синий", GetColorFromString("#2196F3")},
+        };
+        
+        /// <summary>
+        /// Получение цвета по названию
+        /// </summary>
+        /// <param name="colorHex"></param>
+        /// <returns></returns>
+        public static string GetColorName(string colorHex)
+        {
+            Color color = GetColorFromString(colorHex);
+            return ColorsDictionary.SingleOrDefault(c => c.Value == color).Key;
+        }
+        /// <summary>
+        /// Получаем цвет типа Color из строки
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static Color GetColorFromString(string color)
+        {
+            return (Color)ColorConverter.ConvertFromString(color);
+        }
+        /// <summary>
+        /// Проверка строки на цвет
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public static bool IsHexColor(string color)
+        {
+            Regex regex = new Regex(@"^#((([0-9]|[a-f]|[A-F]){6})|(([0-9]|[a-f]|[A-F]){8}))$");
+            return regex.IsMatch(color);
+        }
+    }
+}
