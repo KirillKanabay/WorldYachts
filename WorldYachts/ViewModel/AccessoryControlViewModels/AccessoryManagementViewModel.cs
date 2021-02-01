6a97bc9fbc4ea723541e0840023d5152b6c36e68
Kirill Kanabay <kanabay.dev@gmail.com>
2021-02-01 14:15:34 +0300

    Реализован VM управления аксессуарами

--- WorldYachts/ViewModel/AccessoryControlViewModels/AccessoryManagementViewModel.cs
+++ WorldYachts/ViewModel/AccessoryControlViewModels/AccessoryManagementViewModel.cs
@@ -1,9 +1,40 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using WorldYachts.Data;
+using WorldYachts.Model;
 
 namespace WorldYachts.ViewModel.AccessoryControlViewModels
 {
-    class AccessoryManagementViewModel : BaseViewModel
+    class AccessoryManagementViewModel : BaseManagementViewModel<Accessory>
     {
-        
+        public override IDataModel<Accessory> ModelItem => new AccessoryModel();
+        public override BaseEditorViewModel<Accessory> Editor => new AccessoryEditorViewModel();
+        protected override ObservableCollection<BaseSelectableViewModel<Accessory>> GetSelectableViewModels(IEnumerable<Accessory> items)
+        {
+            var collection = new ObservableCollection<BaseSelectableViewModel<Accessory>>();
+            foreach (var accessory in items)
+            {
+                collection.Add(new SelectableAccessoryViewModel(accessory));
+            }
+
+            return collection;
+        }
+
+        protected override ObservableCollection<BaseSelectableViewModel<Accessory>> Filter(string filterText)
+        {
+            var filteredCollection = ItemsCollection.Where(a =>
+                a.Item.Name.ToLower().Contains(filterText.ToLower()) ||
+                a.Item.Id.ToString() == filterText);
+
+            var accessoryCollection = new ObservableCollection<BaseSelectableViewModel<Accessory>>();
+            foreach (var selectableAccessoryViewModel in filteredCollection)
+            {
+                accessoryCollection.Add(selectableAccessoryViewModel);
+            }
+
+            return accessoryCollection;
+        }
     }
 }
