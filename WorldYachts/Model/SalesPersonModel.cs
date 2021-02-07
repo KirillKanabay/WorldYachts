3a09ed690a9c168c287d4027669056469f8f77aa
Kirill Kanabay <kanabay.dev@gmail.com>
2021-02-07 14:10:44 +0300

    Добавлена модель менеджера

--- WorldYachts/Model/SalesPersonModel.cs
+++ WorldYachts/Model/SalesPersonModel.cs
@@ -1,52 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WorldYachts.Data;
+using WorldYachts.Infrastructure;
 
 namespace WorldYachts.Model
 {
     class SalesPersonModel:IDataModel<SalesPerson>
     {
         public SalesPerson LastAdded { get; set; }
-        public Task AddAsync(SalesPerson item)
+        public async Task AddAsync(SalesPerson item)
         {
-            throw new NotImplementedException();
+            await IsRepeated(item);
+            await using (var context = WorldYachtsContext.GetDataContext())
+            {
+                await context.SalesPersons.AddAsync(item);
+                await context.SaveChangesAsync();
+                LastAdded = item;
+            }
         }
 
-        public Task<IEnumerable<SalesPerson>> LoadAsync()
+        public async Task<IEnumerable<SalesPerson>> LoadAsync()
         {
-            throw new NotImplementedException();
+            return await Task.Run(() => Load());
         }
 
         public IEnumerable<SalesPerson> Load()
         {
-            throw new NotImplementedException();
+            var spCollection = new List<SalesPerson>();
+            using (var context = WorldYachtsContext.GetDataContext())
+            {
+                foreach (var sp in context.SalesPersons)
+                {
+                    sp.Orders = new OrderModel().Load().Where(o => o.SalesPersonId == sp.Id).ToList();
+                    spCollection.Add(sp);
+                }
+            }
+
+            return spCollection;
         }
 
-        public Task RemoveAsync(IEnumerable<SalesPerson> removeItems)
+        public async Task RemoveAsync(IEnumerable<SalesPerson> removeItems)
         {
-            throw new NotImplementedException();
+            await using (var context = WorldYachtsContext.GetDataContext())
+            {
+                context.SalesPersons.RemoveRange(removeItems);
+                await context.SaveChangesAsync();
+            }
         }
 
-        public Task SaveAsync(SalesPerson item)
+        public async Task SaveAsync(SalesPerson item)
         {
-            throw new NotImplementedException();
+            await using (var context = WorldYachtsContext.GetDataContext())
+            {
+                await IsRepeated(item);
+                var dbSP = context.SalesPersons.FirstOrDefault(sp => sp.Id == item.Id);
+
+                dbSP.Name = item.Name;
+                dbSP.SecondName = item.SecondName;
+
+                await context.SaveChangesAsync();
+            }
         }
 
-        public Task IsRepeated(SalesPerson item)
+        public async Task IsRepeated(SalesPerson item)
         {
-            throw new NotImplementedException();
+            
         }
 
-        public Task<SalesPerson> GetItemByIdAsync(int id)
+        public async Task<SalesPerson> GetItemByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await Task.Run((() => GetItemById(id)));
         }
 
         public SalesPerson GetItemById(int id)
         {
-            throw new NotImplementedException();
+            using (var context = WorldYachtsContext.GetDataContext())
+            {
+                return context.SalesPersons.FirstOrDefault(sp => sp.Id == id);
+            }
         }
     }
 }
