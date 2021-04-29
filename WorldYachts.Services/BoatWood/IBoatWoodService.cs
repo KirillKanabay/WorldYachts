49659bfc30190ce99cbe6459976c7078babd3536
Kirill <kirill.kanabay@gmail.com>
2021-04-29 09:15:42 +0300

    Добавлен сервис работы с типом дерева лодок

---
+++ WorldYachts.Services/BoatWood/IBoatWoodService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WorldYachts.Services.BoatWood
+{
+    public interface IBoatWoodService
+    {
+        Task<Data.Entities.BoatWood> GetByIdAsync(int id);
+        Task<IEnumerable<Data.Entities.BoatWood>> GetAllAsync();
+        Task<Data.Entities.BoatWood> AddAsync(Data.Entities.BoatWood boatWood);
+        Task<Data.Entities.BoatWood> UpdateAsync(int id, Data.Entities.BoatWood boatWood);
+        Task<Data.Entities.BoatWood> DeleteAsync(int id);
+    }
+}
