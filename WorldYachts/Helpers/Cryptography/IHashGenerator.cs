cc42b8a26b2709e4ce06bc43f3a7dae452aa0234
Kirill <kirill.kanabay@gmail.com>
2021-05-03 20:57:13 +0300

    Добавлен хеш калькулятор

---
+++ WorldYachts/Helpers/Cryptography/IHashGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldYachts.Helpers.Cryptography
+{
+    public interface IHashCalculator
+    {
+        string GetHash(string value);
+    }
+}
---
+++ WorldYachts/Helpers/Cryptography/Md5HashGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WorldYachts.Helpers.Cryptography
+{
+    public class Md5HashCalculator:IHashCalculator
+    {
+        public string GetHash(string value)
+        {
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(value));
+
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
