a9ea08f885bdf712552bb16be64d90e40d3af5ad
Kirill <kirill.kanabay@gmail.com>
2021-05-04 11:03:41 +0300

    Добавлена модель работы с клиентами

---
+++ WorldYachts.DependencyInjections/Models/ICustomerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WorldYachts.Data.Entities;
+using WorldYachts.Data.ViewModels;
+
+namespace WorldYachts.DependencyInjections.Models
+{
+    public interface ICustomerModel
+    {
+        event Func<object, Task> CustomerModelChanged;
+        Task AddAsync(CustomerUserViewModel customerUserViewModel);
+        Task<IEnumerable<Customer>> GetAllAsync();
+        Task DeleteAsync(Customer customer);
+        Task UpdateAsync(Customer customer);
+        Task<Customer> GetByIdAsync(int id);
+    }
+}
--- WorldYachts/Model/CustomerModel.cs
+++ WorldYachts/Model/CustomerModel.cs
@@ -1,111 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using WorldYachts.Data;
-using WorldYachts.Infrastructure;
+using WorldYachts.Data.ViewModels;
+using WorldYachts.DependencyInjections.Models;
+using WorldYachts.DependencyInjections.Services;
+using Customer = WorldYachts.Data.Entities.Customer;
 
 namespace WorldYachts.Model
 {
-    class CustomerModel:IDataModel<Customer>
+    class CustomerModel:ICustomerModel
     {
-        public Customer LastAddedItem { get; set; }
-        public async Task AddAsync(Customer item)
+        public event Func<object, Task> CustomerModelChanged;
+        private readonly ICustomerService _customerService;
+
+        public CustomerModel(ICustomerService customerService)
         {
-            await IsRepeated(item);
-            await using (var context = WorldYachtsContext.GetDataContext())
-            {
-                await context.Customers.AddAsync(item);
-                await context.SaveChangesAsync();
-                LastAddedItem = item;
-            }
+            _customerService = customerService;
+        }
+
+        public async Task AddAsync(CustomerUserViewModel customerUserViewModel)
+        {
+            await _customerService.AddAsync(customerUserViewModel);
+            CustomerModelChanged?.Invoke(customerUserViewModel);
         }
 
         public async Task<IEnumerable<Customer>> GetAllAsync()
         {
-            return await Task.Run(() => Load());
+            return await _customerService.GetAllAsync();
         }
 
-        public IEnumerable<Customer> Load()
+        public async Task DeleteAsync(Customer customer)
         {
-            var customersCollection = new List<Customer>();
-            using (var context = WorldYachtsContext.GetDataContext())
-            {
-                foreach (var customer in context.Customers.Where(i=>!i.IsDeleted))
-                {
-                    customer.Orders = new OrderModel().Load().Where(o=>o.CustomerId == customer.Id).ToList();
-                    customersCollection.Add(customer);
-                }
-            }
-
-            return customersCollection;
+            await _customerService.DeleteAsync(customer.Id);
+            CustomerModelChanged?.Invoke(customer);
         }
 
-        public async Task DeleteAsync(IEnumerable<Customer> removeItems)
+        public async Task UpdateAsync(Customer customer)
         {
-            await using (var context = WorldYachtsContext.GetDataContext())
-            {
-                foreach (var removeItem in removeItems)
-                {
-                    removeItem.IsDeleted = true;
-                    await UpdateAsync(removeItem);
-                }
-            }
-        }
-
-        public async Task UpdateAsync(Customer item)
-        {
-            await using (var context = WorldYachtsContext.GetDataContext())
-            {
-                await IsRepeated(item);
-                var dbCustomer = context.Customers.FirstOrDefault(c => c.Id == item.Id);
-
-                //Копируем измененного покупателя в БД
-                dbCustomer.Id = item.Id;
-                dbCustomer.Name = item.Name;
-                dbCustomer.SecondName = item.SecondName;
-                dbCustomer.BirthDate = item.BirthDate;
-                dbCustomer.City = item.City;
-                dbCustomer.Address = item.Address;
-                dbCustomer.Phone = item.Phone;
-                dbCustomer.Email = item.Email;
-                dbCustomer.OrganizationName = item.OrganizationName;
-                dbCustomer.IdDocumentName = dbCustomer.IdDocumentName;
-                dbCustomer.IdNumber = dbCustomer.IdNumber;
-                dbCustomer.IsDeleted = item.IsDeleted;
-
-                await context.SaveChangesAsync();
-            }
-        }
-
-        public async Task IsRepeated(Customer item)
-        {
-            await using (var context = WorldYachtsContext.GetDataContext())
-            {
-                //Проверка уникальности почты
-                if (context.Customers.Any(c => c.Email == item.Email))
-                    throw new ArgumentException("Клиент с такой почтой уже существует.");
-                //Проверка уникальности серии документа
-                if (context.Customers.Any(c => c.IdNumber == item.IdNumber))
-                    throw new ArgumentException("Клиент с такими документами уже существует");
-                //Проверка уникальности номера телефона
-                if (context.Customers.Any(c => c.Phone == item.Phone))
-                    throw new ArgumentException("Клиент с таким номером телефона уже существует");
-            }
+            await _customerService.UpdateAsync(customer.Id, customer);
+            CustomerModelChanged?.Invoke(customer);
         }
 
         public async Task<Customer> GetByIdAsync(int id)
         {
-            return await Task.Run((() => GetItemById(id)));
-        }
-
-        public Customer GetItemById(int id)
-        {
-            using (var context = WorldYachtsContext.GetDataContext())
-            { 
-                return context.Customers.FirstOrDefault(c => c.Id == id);
-            }
+            return await _customerService.GetByIdAsync(id);
         }
     }
 }
